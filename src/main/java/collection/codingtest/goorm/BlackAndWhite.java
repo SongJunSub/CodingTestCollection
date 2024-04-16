a2cbe44f87b0c42a89a1d8a614278007df68ee31
SongJunSub <s911130@naver.com>
2024-04-16 18:43:31 +0900

    BlackAndWhite 추가

--- src/main/java/collection/codingtest/goorm/BlackAndWhite.java
+++ src/main/java/collection/codingtest/goorm/BlackAndWhite.java
@@ -1,7 +1,31 @@
 package collection.codingtest.goorm;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class BlackAndWhite {
 
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        String[] firstInput = br.readLine().split(" ");
+        String[] secondInput = br.readLine().split(" ");
+        int[] tempArray = new int[Integer.parseInt(firstInput[1])];
+        int count = 0;
+
+        Arrays.sort(secondInput);
+
+        for(int i=0; i<Integer.parseInt(firstInput[1]); i++){
+            tempArray[i] = Integer.parseInt(secondInput[i]);
+            tempArray[i] = Integer.parseInt(secondInput[0]);
+            count++;
+
+            int tempLength = Integer.parseInt(firstInput[0]) - tempArray.length - 1;
+            System.out.println(tempArray.length);
+        }
+        //System.out.println();
+    }
 
 }
