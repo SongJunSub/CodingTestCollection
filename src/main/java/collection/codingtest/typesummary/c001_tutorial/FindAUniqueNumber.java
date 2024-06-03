998c9dfc93ffc5b6cec61aee5194056d508a494a
Song Jun-Sub <song911130@gmail.com>
2024-06-03 20:26:34 +0900

    Update Find A Unique Number & Solving through HashMap

--- src/main/java/collection/codingtest/typesummary/c001_tutorial/FindAUniqueNumber.java
+++ src/main/java/collection/codingtest/typesummary/c001_tutorial/FindAUniqueNumber.java
@@ -1,6 +1,7 @@
 package collection.codingtest.typesummary.c001_tutorial;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /*
@@ -35,10 +36,41 @@ public class FindAUniqueNumber {
         return list.get(0);
     }
 
+    /*
+        문제 접근
+        HashMap을 사용해서 배열에 들어있는 숫자를 키, 숫자의 등장 횟수를 값으로 저장한다.
+        숫자의 등장 횟수가 1인 키값을 찾아서 리턴한다.
+
+        시간 복잡도 : O(n) + O(n) => O(2n) => O(n)
+        공간 복잡도 : O(n)
+         - HashMap 사이즈가 배열의 크기만큼 늘어날 수 있기 때문에 O(n)이다.
+     */
+    public int solutionWithHashMap(int[] numbers) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        // 시간 복잡도 O(n)
+        for(int number : numbers){
+            // HashMap은 해시 코드를 사용해서 get하기 때문에 전부 다 순회하지 않으므로 시간 복잡도는 O(1)
+            map.put(number, map.getOrDefault(number, 0) + 1);
+        }
+
+        // 시간 복잡도 O(n)
+        for(int number : numbers) {
+            if(map.get(number) == 1) {
+                System.out.println(number);
+
+                return number;
+            }
+        }
+
+        return 0;
+    }
+
     public static void main(String[] args) {
         FindAUniqueNumber findAUniqueNumber = new FindAUniqueNumber();
 
         findAUniqueNumber.solutionWithList(new int[]{2, 3, 1, 3, 2});
+        findAUniqueNumber.solutionWithHashMap(new int[]{2, 3, 1, 3, 2});
     }
 
 }
