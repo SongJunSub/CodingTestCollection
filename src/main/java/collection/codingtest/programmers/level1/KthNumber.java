9e295f02ff9100a71974ade31209c18aa21fea64
Song Jun-Sub <song911130@gmail.com>
2024-06-04 20:08:21 +0900

    Update Kth Number With Better Solution

--- src/main/java/collection/codingtest/programmers/level1/KthNumber.java
+++ src/main/java/collection/codingtest/programmers/level1/KthNumber.java
@@ -52,8 +52,23 @@ public class KthNumber {
         return answer;
     }
 
+    public int[] betterSolution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for (int i = 0; i < commands.length; i++) {
+            int[] temp = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+
+            Arrays.sort(temp);
+
+            answer[i] = temp[commands[i][2] - 1];
+        }
+
+        return answer;
+    }
+
     public static void main(String[] args) {
         System.out.println(Arrays.toString(new KthNumber().solution(new int[]{1, 5, 2, 6, 3, 7, 4}, new int[][]{{2, 5, 3}, {4, 4, 1}, {1, 7, 3}})));
+        System.out.println(Arrays.toString(new KthNumber().betterSolution(new int[]{1, 5, 2, 6, 3, 7, 4}, new int[][]{{2, 5, 3}, {4, 4, 1}, {1, 7, 3}})));
     }
 
 }
