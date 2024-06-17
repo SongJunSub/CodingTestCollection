e80dd5d2178e13c95ba0bc2b4f3049bade065557
Song Jun-Sub <song911130@gmail.com>
2024-06-17 16:15:01 +0900

    Update Templar's Weapon With Better Solution

--- src/main/java/collection/codingtest/programmers/level1/TemplarsWeapon.java
+++ src/main/java/collection/codingtest/programmers/level1/TemplarsWeapon.java
@@ -52,9 +52,39 @@ public class TemplarsWeapon {
         return answer;
     }
 
+    public int betterSolution(int number, int limit, int power) {
+        int answer = 0;
+
+        for (int i = 1; i <= number; i++) {
+            int measureCount = 0;
+
+            for (int j = 1; j * j <= i; j++) {
+                if (i % j == 0) {
+                    measureCount++;
+
+                    if (j != i / j) {
+                        measureCount++;
+                    }
+                }
+            }
+
+            if (measureCount > limit) {
+                answer += power;
+            }
+            else {
+                answer += measureCount;
+            }
+        }
+
+        return answer;
+    }
+
     public static void main(String[] args) {
         System.out.println(new TemplarsWeapon().solution(5, 3, 2));
         System.out.println(new TemplarsWeapon().solution(10, 3, 2));
+
+        System.out.println(new TemplarsWeapon().betterSolution(5, 3, 2));
+        System.out.println(new TemplarsWeapon().betterSolution(10, 3, 2));
     }
 
 }
