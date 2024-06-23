bcdfff50eb21520d31ecd7c79d479d7b0e8d7240
Song Jun-Sub <song911130@gmail.com>
2024-06-23 13:14:07 +0900

    Add Highest And Lowest Lotto Rankings With My Solution

--- src/main/java/collection/codingtest/programmers/level1/DartGame.java
+++ src/main/java/collection/codingtest/programmers/level1/DartGame.java
@@ -42,14 +42,72 @@ public class DartGame {
         return answer;
     }
 
+    public int betterSolution(String dartResult) {
+        int answer = 0;
+        int i = 0;
+
+        while (i < dartResult.length()) {
+            int result = 0;
+
+            if (Character.isDigit(dartResult.charAt(i))) {
+                // 정수 10 체크
+                if (i + 1 < dartResult.length() && dartResult.charAt(i) == '1' && dartResult.charAt(i + 1) == '0') {
+                    result = 10;
+                    i += 2;
+                }
+                else {
+                    result = dartResult.charAt(i) - '0';
+                    i++;
+                }
+            }
+
+            if (i < dartResult.length()) {
+                char bonus = dartResult.charAt(i);
+                if (bonus == 'S') {
+                    result = (int) Math.pow(result, 1);
+                } else if (bonus == 'D') {
+                    result = (int) Math.pow(result, 2);
+                } else if (bonus == 'T') {
+                    result = (int) Math.pow(result, 3);
+                }
+                i++;
+            }
+
+            // Check for option
+            if (i < dartResult.length()) {
+                char option = dartResult.charAt(i);
+                if (option == '*' || option == '#') {
+                    if (option == '*') {
+                        result *= 2;
+                    } else if (option == '#') {
+                        result *= -1;
+                    }
+                    i++;
+                }
+            }
+
+            answer += result;
+        }
+
+        return answer;
+    }
+
     public static void main(String[] args) {
-        System.out.println(new DartGame().solution("1S2D*3T"));
+//        System.out.println(new DartGame().solution("1S2D*3T"));
 //        System.out.println(new DartGame().solution("1D2S#10S"));
 //        System.out.println(new DartGame().solution("1D2S0T"));
 //        System.out.println(new DartGame().solution("1S*2T*3S"));
 //        System.out.println(new DartGame().solution("1D#2S*3S"));
 //        System.out.println(new DartGame().solution("1T2D3D#"));
 //        System.out.println(new DartGame().solution("1D2S3T*"));
+        System.out.println("\n");
+        System.out.println(new DartGame().betterSolution("1S2D*3T"));
+        System.out.println(new DartGame().betterSolution("1D2S#10S"));
+        System.out.println(new DartGame().betterSolution("1D2S0T"));
+        System.out.println(new DartGame().betterSolution("1S*2T*3S"));
+        System.out.println(new DartGame().betterSolution("1D#2S*3S"));
+        System.out.println(new DartGame().betterSolution("1T2D3D#"));
+        System.out.println(new DartGame().betterSolution("1D2S3T*"));
     }
 
 }
